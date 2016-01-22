7565cf8957e5e8746fddf5d0f45d6da5cc48dd64
cland29 <carl.lee.landskron@gmail.com>
2016-01-21 20:02:08 -0500

    Added two CIMs to Shooter

--- src/org/usfirst/frc/team1747/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1747/robot/subsystems/Shooter.java
@@ -5,19 +5,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Shooter extends Subsystem{
 	
-	CANTalon leftShooter;
-	CANTalon rightShooter;
+	CANTalon leftShooter1, leftShooter2;
+	CANTalon rightShooter1, rightShooter2;
 	
 	public Shooter(){
 		System.out.println("Shooter created");
-		leftShooter = new CANTalon(1);
-		rightShooter = new CANTalon(22);
+		leftShooter1 = new CANTalon(1);
+		rightShooter1 = new CANTalon(22);
+		leftShooter2 = new CANTalon(11);
+		rightShooter2 = new CANTalon(2);
+		
 	}
 	
 	public void shoot(double speed){
 		System.out.println("Shooting" + speed);
-		leftShooter.set(-speed);
-		rightShooter.set(speed);
+		leftShooter1.set(-speed);
+		leftShooter2.set(-speed);
+		rightShooter1.set(speed);
+		rightShooter2.set(speed);
 	}
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
