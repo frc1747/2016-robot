9fe84d430ea66015efa8d1418ee8419db1f86221
dfroh40 <dmfrohberg@gmail.com>
2016-01-27 19:18:04 -0500

    Rename smoothDrive variables and use a LinkedList

--- src/org/usfirst/frc/team1747/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1747/robot/subsystems/DriveTrain.java
@@ -1,31 +1,24 @@
 package org.usfirst.frc.team1747.robot.subsystems;
 
+import java.util.LinkedList;
+
 import org.usfirst.frc.team1747.robot.RobotMap;
 import org.usfirst.frc.team1747.robot.commands.TeleopDrive;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
 public class DriveTrain extends Subsystem {
-	
-	CANTalon leftCimOne;
-	CANTalon leftCimTwo;
-	CANTalon leftMiniCim;
-	CANTalon rightCimOne;
-	CANTalon rightCimTwo;
-	CANTalon rightMiniCim;
-	
-	static final double[] SIGMOIDSTRETCH = {0.03, 0.06, 0.09, 0.1, 
-			0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03};
-	
-	double[] leftTargetDeltas = new double[SIGMOIDSTRETCH.length];
-	double[] rightTargetDeltas = new double[SIGMOIDSTRETCH.length];
-	
-	double pLeftCurrent, pRightCurrent, prevLeftTarget, prevRightTarget;
+	CANTalon leftCimOne, leftCimTwo, leftMiniCim;
+	CANTalon rightCimOne, rightCimTwo, rightMiniCim;
 
+	static final double[] SIGMOIDSTRETCH = { 0.03, 0.06, 0.09, 0.1, 0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03 };
+	LinkedList<Double> straightTargetDeltas = new LinkedList<Double>();
+	LinkedList<Double> rotationTargetDeltas = new LinkedList<Double>();
+
+	double pStraightTarget=0.0, pRotationTarget=0.0, prevTargetStraight=0.0, prevTargetRotation=0.0;
+
+	// Sets up CANTalons for drive train
 	public DriveTrain() {
 		leftCimOne = new CANTalon(RobotMap.LEFT_DRIVE_CIM_ONE);
 		leftCimTwo = new CANTalon(RobotMap.LEFT_DRIVE_CIM_TWO);
@@ -33,17 +26,18 @@ public class DriveTrain extends Subsystem {
 		rightCimOne = new CANTalon(RobotMap.RIGHT_DRIVE_CIM_ONE);
 		rightCimTwo = new CANTalon(RobotMap.RIGHT_DRIVE_CIM_TWO);
 		rightMiniCim = new CANTalon(RobotMap.RIGHT_DRIVE_MINICIM);
-		for(int j = 0; j < SIGMOIDSTRETCH.length; j++){
-			leftTargetDeltas[j] = 0.0;
-			rightTargetDeltas[j] = 0.0;
+		// Left and right motors face each other
+		rightCimOne.setInverted(true);
+		rightCimTwo.setInverted(true);
+		rightMiniCim.setInverted(true);
+		for (int j = 0; j < SIGMOIDSTRETCH.length; j++) {
+			straightTargetDeltas.add(0.0);
+			rotationTargetDeltas.add(0.0);
 		}
-		prevLeftTarget = 0.0;
-		prevRightTarget = 0.0;
-		pLeftCurrent = 0.0;
-		pRightCurrent = 0.0;
 	}
 
-	public void tankDrive(double leftSpeed, double rightSpeed){
+	// Sets up the tank drive.
+	public void tankDrive(double leftSpeed, double rightSpeed) {
 		leftCimOne.set(leftSpeed);
 		leftCimTwo.set(leftSpeed);
 		leftMiniCim.set(leftSpeed);
@@ -51,32 +45,33 @@ public class DriveTrain extends Subsystem {
 		rightCimTwo.set(rightSpeed);
 		rightMiniCim.set(rightSpeed);
 	}
-	
-	public void arcadeDrive(double straight, double turn){
-			tankDrive(straight+turn, straight-turn);
+
+	public void arcadeDrive(double straight, double turn) {
+		tankDrive(straight + turn, straight - turn);
 	}
-	
-	public void smoothDrive(double targetLeftCurrent, double targetRightCurrent){
-		for(int i = leftTargetDeltas.length-1; i > 0; i--){
-			leftTargetDeltas[i] = leftTargetDeltas[i-1];
-			rightTargetDeltas[i] = rightTargetDeltas[i-1];
+
+	// This is smooth drive. bush did 9II
+	public void smoothDrive(double targetStraight, double targetRotation) {
+		straightTargetDeltas.removeLast();
+		rotationTargetDeltas.removeLast();
+		straightTargetDeltas.addFirst(targetStraight - prevTargetStraight);
+		rotationTargetDeltas.addFirst(targetRotation - prevTargetRotation);
+		prevTargetStraight = targetStraight;
+		prevTargetRotation = targetRotation;
+		for (int i = 0; i < SIGMOIDSTRETCH.length; i++) {
+			pStraightTarget += straightTargetDeltas.get(i) * SIGMOIDSTRETCH[i];
+			pRotationTarget += rotationTargetDeltas.get(i) * SIGMOIDSTRETCH[i];
 		}
-		leftTargetDeltas[0] = targetLeftCurrent - prevLeftTarget;
-		rightTargetDeltas[0] = targetRightCurrent - prevRightTarget;
-		prevLeftTarget = targetLeftCurrent;
-		prevRightTarget = targetRightCurrent;
-		for(int i = 0; i< SIGMOIDSTRETCH.length; i++){
-			pLeftCurrent += leftTargetDeltas[i]*SIGMOIDSTRETCH[i];
-			pRightCurrent += rightTargetDeltas[i]*SIGMOIDSTRETCH[i];
-		}
-		arcadeDrive(pLeftCurrent, pRightCurrent);
+		arcadeDrive(pStraightTarget, pRotationTarget);
 	}
 
 	public void initDefaultCommand() {
 		setDefaultCommand(new TeleopDrive());
-		
+
 	}
 
+	// This is a public void that logs smart dashboard.
 	public void logToSmartDashboard() {
 	}
 }
+
