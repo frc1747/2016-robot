2719d8c2a279cefda251a855d1f20e9972d74d42
dfroh40 <dmfrohberg@gmail.com>
2016-01-30 10:21:16 -0500

    Deleted extraneous code

---
+++ src/org/usfirst/frc/team1747/robot/commands/BallEject.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team1747.robot.commands;
+
+import org.usfirst.frc.team1747.robot.Robot;
+import org.usfirst.frc.team1747.robot.subsystems.Intake;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class BallEject extends Command {
+
+	Intake intake;
+	double startTime;
+
+	public BallEject() {
+		intake = Robot.getIntake();
+		requires(intake);
+	}
+
+	protected void initialize() {
+		startTime = System.currentTimeMillis();
+	}
+
+	// Eject the ball
+	protected void execute() {
+		intake.ejectBall();
+	}
+
+	// After 2000milliseconds, stop the motors
+	protected boolean isFinished() {
+		return (System.currentTimeMillis() - startTime >= 2000);
+	}
+
+	protected void end() {
+		intake.rollerControl(0.0);
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
+}
