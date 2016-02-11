1991bb7b77ab05d5ae3aed8e33d02f0b78ac4ba2
Anubhaw Arya <anubhaw.arya@gmail.com>
2016-02-10 19:18:53 -0500

    Added PID controller to DriveTrain

--- src/org/usfirst/frc/team1747/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1747/robot/subsystems/DriveTrain.java
@@ -1,125 +1,181 @@
 package org.usfirst.frc.team1747.robot.subsystems;
 
-import java.util.LinkedList;
-
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team1747.robot.RobotMap;
 import org.usfirst.frc.team1747.robot.SDLogger;
 import org.usfirst.frc.team1747.robot.commands.TeleopDrive;
 
-import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.LinkedList;
 
 public class DriveTrain extends Subsystem implements SDLogger {
-	CANTalon leftCimOne, leftCimTwo, leftMiniCim;
-	CANTalon rightCimOne, rightCimTwo, rightMiniCim;
 
-	static final double[] SIGMOIDSTRETCH = { 0.03, 0.06, 0.09, 0.1, 0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03 };
+    class DriveSide {
+        CANTalon cimOne, cimTwo, miniCim;
+        double kP, kI, kD;
+        boolean pidEnabled;
+        double targetSpeed;
+        double integralError;
+        double previousError;
 
-	LinkedList<Double> straightTargetDeltas = new LinkedList<Double>();
-	LinkedList<Double> rotationTargetDeltas = new LinkedList<Double>();
 
-	double pStraightTarget = 0.0, pRotationTarget = 0.0, prevTargetStraight = 0.0, prevTargetRotation = 0.0;
+        public DriveSide(int cimOneID, int cimTwoID, int miniCimID, boolean inverted) {
+            cimOne = new CANTalon(cimOneID);
+            cimTwo = new CANTalon(cimTwoID);
+            miniCim = new CANTalon(miniCimID);
+            cimOne.setInverted(inverted);
+            cimTwo.setInverted(inverted);
+            miniCim.setInverted(inverted);
+            pidEnabled = false;
+            targetSpeed = 0;
+            integralError = 0;
+            previousError = 0;
+        }
 
-	private static final double kP = 0;
-	private static final double kI = 0;
-	private static final double kD = 0;
+        public double getP() {
+            return kP;
+        }
 
-	// Sets up CANTalons for drive train
-	public DriveTrain() {
-		leftCimOne = new CANTalon(RobotMap.LEFT_DRIVE_CIM_ONE);
-		setupPID(leftCimOne, CANTalon.TalonControlMode.Position);
-		leftCimTwo = new CANTalon(RobotMap.LEFT_DRIVE_CIM_TWO);
-		setupPID(leftCimTwo, CANTalon.TalonControlMode.Follower);
-		leftMiniCim = new CANTalon(RobotMap.LEFT_DRIVE_MINICIM);
-		setupPID(leftMiniCim, CANTalon.TalonControlMode.Follower);
-		rightCimOne = new CANTalon(RobotMap.RIGHT_DRIVE_CIM_ONE);
-		setupPID(rightCimOne, CANTalon.TalonControlMode.Position);
-		rightCimTwo = new CANTalon(RobotMap.RIGHT_DRIVE_CIM_TWO);
-		setupPID(rightCimTwo, CANTalon.TalonControlMode.Follower);
-		rightMiniCim = new CANTalon(RobotMap.RIGHT_DRIVE_MINICIM);
-		setupPID(rightMiniCim, CANTalon.TalonControlMode.Follower);
-		// Left and right motors face each other
-		leftCimOne.setInverted(true);
-		leftCimTwo.setInverted(true);
-		leftMiniCim.setInverted(true);
-		for (int j = 0; j < SIGMOIDSTRETCH.length; j++) {
-			straightTargetDeltas.add(0.0);
-			rotationTargetDeltas.add(0.0);
-		}
-	}
+        public double getI() {
+            return kI;
+        }
 
-	// Sets up the tank drive.
-	public void tankDrive(double leftSpeed, double rightSpeed) {
-		leftCimOne.set(leftSpeed);
-		leftCimTwo.set(leftSpeed);
-		leftMiniCim.set(leftSpeed);
-		rightCimOne.set(rightSpeed);
-		rightCimTwo.set(rightSpeed);
-		rightMiniCim.set(rightSpeed);
-	}
+        public double getD() {
+            return kD;
+        }
 
-	public void arcadeDrive(double straight, double turn) {
-		tankDrive(straight + turn, straight - turn);
-	}
 
-	// This is smooth drive. bush did 9II
-	public void smoothDrive(double targetStraight, double targetRotation) {
-		straightTargetDeltas.removeLast();
-		rotationTargetDeltas.removeLast();
-		straightTargetDeltas.addFirst(targetStraight - prevTargetStraight);
-		rotationTargetDeltas.addFirst(targetRotation - prevTargetRotation);
-		prevTargetStraight = targetStraight;
-		prevTargetRotation = targetRotation;
-		for (int i = 0; i < SIGMOIDSTRETCH.length; i++) {
-			pStraightTarget += straightTargetDeltas.get(i) * SIGMOIDSTRETCH[i];
-			pRotationTarget += rotationTargetDeltas.get(i) * SIGMOIDSTRETCH[i];
-		}
-		arcadeDrive(pStraightTarget, pRotationTarget);
-	}
+        @SuppressWarnings("Duplicates")
+        public void runPID() {
+            double currentSpeed = getSpeed();
+            double currentError = (currentSpeed - targetSpeed);
+            integralError += currentError;
+            // Motor Voltage = Kp*error + Ki*error_sum + Kd*(error-error_last)
+            double speed = kP * currentError + kI * integralError + kD * (currentError - previousError);
+            previousError = currentError;
+            if (cimOne.getInverted())
+                SmartDashboard.putNumber("left", speed);
+            else
+                SmartDashboard.putNumber("right", speed);
+            set(speed);
+        }
 
-	public void plateauDrive(double straight, double turn) {
-		if (Math.abs(straight) < .01) {
-			straight = 0.0;
-		} else if (Math.abs(straight) < 0.5) {
-			straight = (Math.abs(straight) / straight) * 0.5 / (1 + Math.exp(-20.0 * (straight - 0.2)));
-		} else {
-			straight = (Math.abs(straight) / straight) * (0.5 / (1 + Math.exp(-20.0 * (straight - 0.8)) + 0.5));
-		}
-		if (Math.abs(turn) < .01) {
-			turn = 0.0;
-		} else if (Math.abs(turn) < 0.5) {
-			turn = (Math.abs(turn) / turn) * 0.5 / (1 + Math.exp(-20.0 * (turn - 0.2)));
-		} else {
-			turn = (Math.abs(turn) / turn) * (0.5 / (1 + Math.exp(-20.0 * (turn - 0.8)) + 0.5));
-		}
-		SmartDashboard.putNumber("Straight", straight);
-		SmartDashboard.putNumber("Turn", turn);
-		tankDrive(straight + turn, straight - turn);
-	}
+        public double getSpeed() {
+            return cimTwo.getSpeed();
+        }
 
-	public void setupPID(CANTalon talon, CANTalon.TalonControlMode controlMode) {
-		// talon.setControlMode(controlMode.getValue());
-		// talon.setPID(kP, kI, kD);
-	}
+        public void set(double speed) {
+            cimOne.set(speed);
+            cimTwo.set(speed);
+            miniCim.set(speed);
+        }
 
-	public double getLeftSpeed() {
-		return leftCimTwo.getSpeed();
-	}
+        public void setPID(double p, double i, double d) {
+            kP = p;
+            kI = i;
+            kD = d;
+        }
 
-	public double getRightSpeed() {
-		return -rightCimTwo.getSpeed();
-	}
+        public void setSetpoint(double targetSpeed) {
+            this.targetSpeed = targetSpeed;
+        }
 
-	@Override
-	public void initDefaultCommand() {
-		setDefaultCommand(new TeleopDrive());
+        public void enablePID() {
+            pidEnabled = true;
+        }
 
-	}
+        public void disablePID() {
+            pidEnabled = false;
+            targetSpeed = 0;
+            integralError = 0;
+            previousError = 0;
+        }
+    }
 
-	// This is a public void that logs smart dashboard.
-	public void logToSmartDashboard() {
-		SmartDashboard.putNumber("Left DriveTrain Speed", getLeftSpeed());
-		SmartDashboard.putNumber("Right DriveTrain Speed", getRightSpeed());
-	}
+    DriveSide left, right;
+
+
+    static final double[] SIGMOIDSTRETCH = {0.03, 0.06, 0.09, 0.1, 0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03};
+
+    LinkedList<Double> straightTargetDeltas = new LinkedList<Double>();
+    LinkedList<Double> rotationTargetDeltas = new LinkedList<Double>();
+
+    double pStraightTarget = 0.0, pRotationTarget = 0.0, prevTargetStraight = 0.0, prevTargetRotation = 0.0;
+
+    // Sets up CANTalons for drive train
+    public DriveTrain() {
+        left = new DriveSide(RobotMap.LEFT_DRIVE_CIM_ONE, RobotMap.LEFT_DRIVE_CIM_TWO, RobotMap.LEFT_DRIVE_MINICIM,
+                true);
+        right = new DriveSide(RobotMap.RIGHT_DRIVE_CIM_ONE, RobotMap.RIGHT_DRIVE_CIM_TWO, RobotMap
+                .RIGHT_DRIVE_MINICIM, false);
+        // Left and right motors face each other, left is inverted
+        for (int j = 0; j < SIGMOIDSTRETCH.length; j++) {
+            straightTargetDeltas.add(0.0);
+            rotationTargetDeltas.add(0.0);
+        }
+    }
+
+    // Sets up the tank drive.
+    public void tankDrive(double leftSpeed, double rightSpeed) {
+        left.set(leftSpeed);
+        right.set(rightSpeed);
+    }
+
+    public void arcadeDrive(double straight, double turn) {
+        tankDrive(straight + turn, straight - turn);
+    }
+
+    // This is smooth drive. bush did 9II
+    public void smoothDrive(double targetStraight, double targetRotation) {
+        straightTargetDeltas.removeLast();
+        rotationTargetDeltas.removeLast();
+        straightTargetDeltas.addFirst(targetStraight - prevTargetStraight);
+        rotationTargetDeltas.addFirst(targetRotation - prevTargetRotation);
+        prevTargetStraight = targetStraight;
+        prevTargetRotation = targetRotation;
+        for (int i = 0; i < SIGMOIDSTRETCH.length; i++) {
+            pStraightTarget += straightTargetDeltas.get(i) * SIGMOIDSTRETCH[i];
+            pRotationTarget += rotationTargetDeltas.get(i) * SIGMOIDSTRETCH[i];
+        }
+        arcadeDrive(pStraightTarget, pRotationTarget);
+    }
+
+    @SuppressWarnings("Duplicates")
+    public void plateauDrive(double straight, double turn) {
+        if (Math.abs(straight) < .01) {
+            straight = 0.0;
+        } else if (Math.abs(straight) < 0.5) {
+            straight = (Math.abs(straight) / straight) * 0.5 / (1 + Math.exp(-20.0 * (Math.abs(straight) - 0.2)));
+        } else {
+            straight = (Math.abs(straight) / straight) * (0.5 / (1 + Math.exp(-20.0 * (Math.abs(straight) - 0.8)) + 0.5));
+        }
+        if (Math.abs(turn) < .01) {
+            turn = 0.0;
+        } else if (Math.abs(turn) < 0.5) {
+            turn = (Math.abs(turn) / turn) * 0.5 / (1 + Math.exp(-20.0 * (Math.abs(turn) - 0.2)));
+        } else {
+            turn = (Math.abs(turn) / turn) * (0.5 / (1 + Math.exp(-20.0 * (Math.abs(turn) - 0.8)) + 0.5));
+        }
+        SmartDashboard.putNumber("Straight", straight);
+        SmartDashboard.putNumber("Turn", turn);
+        tankDrive(straight + turn, straight - turn);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        setDefaultCommand(new TeleopDrive());
+    }
+
+    // This is a public void that logs smart dashboard.
+    public void logToSmartDashboard() {
+        SmartDashboard.putNumber("Left DriveTrain Speed", left.getSpeed());
+        SmartDashboard.putNumber("Right DriveTrain Speed", right.getSpeed());
+        left.setPID(SmartDashboard.getNumber("DriveTrain LP", left.getP()),
+                SmartDashboard.getNumber("DriveTrain LI", left.getI()),
+                SmartDashboard.getNumber("DriveTrain LD", left.getD()));
+        right.setPID(SmartDashboard.getNumber("DriveTrain RP", right.getP()),
+                SmartDashboard.getNumber("DriveTrain RI", right.getI()),
+                SmartDashboard.getNumber("DriveTrain RD", right.getD()));
+    }
 }
