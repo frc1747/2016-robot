c6a83702c8d48ec651b27f232b0510de55002ff2
Anubhaw Arya <anubhaw.arya@gmail.com>
2016-02-15 16:34:11 -0500

    Turning based on position

--- src/org/usfirst/frc/team1747/robot/SDController.java
+++ src/org/usfirst/frc/team1747/robot/SDController.java
@@ -1,34 +1,47 @@
 package org.usfirst.frc.team1747.robot;
 
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team1747.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team1747.robot.subsystems.Intake;
 import org.usfirst.frc.team1747.robot.subsystems.Shooter;
 
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 public class SDController {
 
-	private OI oi;
-	private DriveTrain driveTrain;
-	private Shooter shooter;
-	private Intake intake;
-	private NetworkTable networkTable = NetworkTable.getTable("imageProcessing");
+    SendableChooser autonPosition;
+    private OI oi;
+    private DriveTrain driveTrain;
+    private Shooter shooter;
+    private Intake intake;
+    private NetworkTable networkTable = NetworkTable.getTable("imageProcessing");
 
-	public SDController() {
-		oi = Robot.getOi();
-		driveTrain = Robot.getDrive();
-		shooter = Robot.getShooter();
-		intake = Robot.getIntake();
-		SmartDashboard.putData(Scheduler.getInstance());
-	}
+    public SDController() {
+        oi = Robot.getOi();
+        driveTrain = Robot.getDrive();
+        shooter = Robot.getShooter();
+        intake = Robot.getIntake();
+        SmartDashboard.putData(Scheduler.getInstance());
+        autonPosition = new SendableChooser();
+        autonPosition.addDefault("Don't shoot", 0);
+        autonPosition.addObject("1", 1);
+        autonPosition.addObject("2", 2);
+        autonPosition.addObject("3", 3);
+        autonPosition.addObject("4", 4);
+        autonPosition.addObject("5", 5);
+        SmartDashboard.putData("Auton Position", autonPosition);
+    }
 
-	public void refresh() {
-		//oi.getController().logToSmartDashboard();
-		driveTrain.logToSmartDashboard();
-		shooter.logToSmartDashboard();
-		intake.logToSmartDashboard();
-		SmartDashboard.putString("ShooterDirection", networkTable.getString("ShootDirection", "robotUnknown"));
-	}
+    public void refresh() {
+        //oi.getController().logToSmartDashboard();
+        driveTrain.logToSmartDashboard();
+        shooter.logToSmartDashboard();
+        intake.logToSmartDashboard();
+        SmartDashboard.putString("ShooterDirection", networkTable.getString("ShootDirection", "robotUnknown"));
+    }
+
+    public int getAutonPosition() {
+        return (int) autonPosition.getSelected();
+    }
 }
--- src/org/usfirst/frc/team1747/robot/commands/AutoShoot.java
+++ src/org/usfirst/frc/team1747/robot/commands/AutoShoot.java
@@ -9,79 +9,87 @@ import org.usfirst.frc.team1747.robot.subsystems.Shooter;
 
 public class AutoShoot extends Command {
 
-	DriveTrain drive;
-	Shooter shoot;
-	Intake intake;
-	NetworkTable networkTable;
-	double startTime;
+    DriveTrain drive;
+    Shooter shoot;
+    Intake intake;
+    NetworkTable networkTable;
+    double startTime;
+    int position;
 
-	public AutoShoot() {
-		drive = Robot.getDrive();
-		shoot = Robot.getShooter();
-		intake = Robot.getIntake();
-		networkTable = NetworkTable.getTable("imageProcessing");
-		requires(shoot);
-		requires(drive);
-		requires(intake);
-	}
+    public AutoShoot() {
+        drive = Robot.getDrive();
+        shoot = Robot.getShooter();
+        intake = Robot.getIntake();
+        networkTable = NetworkTable.getTable("imageProcessing");
+        requires(shoot);
+        requires(drive);
+        requires(intake);
+        position = Robot.getSd().getAutonPosition();
+    }
 
-	protected void initialize() {
-		startTime = -1;
-		System.out.println("Running");
-	}
+    protected void initialize() {
+        startTime = -1;
+        System.out.println("Running");
+    }
 
-	protected void execute() {
-		String direction = networkTable.getString("ShootDirection", "robotUnknown");
-		switch (direction) {
-			case "left":
-				shoot.shoot(0);
-				drive.arcadeDrive(0.0, -0.225);
-				startTime = -1;
-				break;
-			case "right":
-				shoot.shoot(0);
-				drive.arcadeDrive(0.0, 0.225);
-				startTime = -1;
-				break;
-			case "forward":
-				shoot.shoot(0);
-				drive.arcadeDrive(0.25, 0.0);
-				startTime = -1;
-				break;
-			case "backward":
-				shoot.shoot(0);
-				drive.arcadeDrive(-0.25, 0.0);
-				startTime = -1;
-				break;
-			case "shoot":
-				drive.arcadeDrive(0, 0);
-				if (startTime == -1) {
-					startTime = System.currentTimeMillis();
-				}
-				if (startTime != -1 && System.currentTimeMillis() - startTime > 500) {
-					shoot.shoot(0.6);
-				}
-				if (startTime != -1 && System.currentTimeMillis() - startTime > 3000) {
-					intake.intakeBall();
-				}
-				break;
-			case "unknown":
-				drive.arcadeDrive(0, -0.3);
-				break;
-		}
-	}
+    protected void execute() {
+        if (position == 0)
+            return;
+        String direction = networkTable.getString("ShootDirection", "robotUnknown");
+        switch (direction) {
+            case "left":
+                shoot.shoot(0);
+                drive.arcadeDrive(0.0, -0.225);
+                startTime = -1;
+                break;
+            case "right":
+                shoot.shoot(0);
+                drive.arcadeDrive(0.0, 0.225);
+                startTime = -1;
+                break;
+            case "forward":
+                shoot.shoot(0);
+                drive.arcadeDrive(0.25, 0.0);
+                startTime = -1;
+                break;
+            case "backward":
+                shoot.shoot(0);
+                drive.arcadeDrive(-0.25, 0.0);
+                startTime = -1;
+                break;
+            case "shoot":
+                drive.arcadeDrive(0, 0);
+                if (startTime == -1) {
+                    startTime = System.currentTimeMillis();
+                }
+                if (startTime != -1 && System.currentTimeMillis() - startTime > 500) {
+                    shoot.shoot(0.6);
+                }
+                if (startTime != -1 && System.currentTimeMillis() - startTime > 3000) {
+                    intake.intakeBall();
+                }
+                break;
+            case "unknown":
+                if (position <= 3) {
+                    drive.arcadeDrive(0, 0.3);
+                } else {
+                    drive.arcadeDrive(0, -0.3);
+                }
+                break;
+        }
+    }
 
-	protected boolean isFinished() {
-		return startTime != -1 && System.currentTimeMillis() - startTime > 4000;
-	}
+    protected boolean isFinished() {
+        return (startTime != -1 && System.currentTimeMillis() - startTime > 4000) || position == 0;
+    }
 
-	protected void end() {
-		drive.arcadeDrive(0, 0);
-		shoot.shoot(0);
-	}
+    protected void end() {
+        drive.arcadeDrive(0, 0);
+        shoot.shoot(0);
+    }
 
-	protected void interrupted() {
-		end();
-	}
+    protected void interrupted() {
+        end();
+    }
 
 }
