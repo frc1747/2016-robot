adc99eebb97f9ea86402b698a7105fc25b5c7517
dfroh40 <dmfrohberg@gmail.com>
2016-03-26 12:14:48 -0400

    Adjusted autoshoot timing

--- src/org/usfirst/frc/team1747/robot/commands/AutoShoot.java
+++ src/org/usfirst/frc/team1747/robot/commands/AutoShoot.java
@@ -140,7 +140,7 @@ public class AutoShoot extends Command {
 	// returns true if auto mode is done, if not it returns false; uses
 	// startTime, position, and the current system time
 	protected boolean isFinished() {
-		return (startTime != -1 && System.currentTimeMillis() - startTime > 4000) || position == 0;
+		return (startTime != -1 && System.currentTimeMillis() - startTime > 2250) || position == 0;
 	}
 
 	// ends shoot and arcadeDrive
