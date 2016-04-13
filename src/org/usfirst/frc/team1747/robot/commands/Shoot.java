5fa400ccf66a9f0ea5a08d87b3922196e13f370f
Anubhaw Arya <anubhaw.arya@gmail.com>
2016-04-13 14:00:23 -0400

    Default shoot command to PID

--- src/org/usfirst/frc/team1747/robot/commands/Shoot.java
+++ src/org/usfirst/frc/team1747/robot/commands/Shoot.java
@@ -1,19 +1,15 @@
 package org.usfirst.frc.team1747.robot.commands;
 
+import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team1747.robot.Robot;
 import org.usfirst.frc.team1747.robot.subsystems.Intake;
 import org.usfirst.frc.team1747.robot.subsystems.Shooter;
-
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shoot extends Command {
 
 	private Shooter shooter;
 	private Intake intake;
-	private boolean pidMode;
 	private long startTime = -1;
-	private double speed;
 
 	public Shoot() {
 		shooter = Robot.getShooter();
@@ -22,54 +18,35 @@ public class Shoot extends Command {
 		requires(intake);
 	}
 
-	@Override
 	protected void initialize() {
-		pidMode = SmartDashboard.getBoolean("Shooter PID Mode", true);
-		speed = SmartDashboard.getNumber("Target Shooter Speed", .65);
 		startTime = -1;
-		if (pidMode) {
-			shooter.enablePID();
-			shooter.setSetpoint(speed);
-		} else {
-			shooter.shoot(speed);
-		}
+		shooter.setSetpoint(shooter.getTargetShooterSpeed());
+		shooter.enablePID();
 	}
 
-	@Override
 	protected void execute() {
-		if (pidMode) {
-			shooter.runPID();
-			if (shooter.isAtTarget()) {
-				if (startTime == -1) {
-					startTime = System.currentTimeMillis();
-				} else if (System.currentTimeMillis() - startTime > 500) {
-					intake.intakeBall();
-				}
-			} else {
-				startTime = -1;
+		shooter.runPID();
+		if (shooter.isAtTarget()) {
+			if (startTime == -1) {
+				startTime = System.currentTimeMillis();
+			} else if (System.currentTimeMillis() - startTime > 500) {
+				intake.intakeBall();
 			}
-		} else if (shooter.getLeftSpeed() > speed && shooter.getRightSpeed() > speed) {
-			// intake.intakeBall();
-			// startTime = System.currentTimeMillis();
+		} else {
+			startTime = -1;
 		}
-
 	}
 
-	@Override
 	protected boolean isFinished() {
 		return startTime != -1 && System.currentTimeMillis() - startTime > 2000;
 	}
 
-	@Override
 	protected void end() {
-		if (pidMode) {
-			shooter.disablePID();
-		}
+		shooter.disablePID();
 		shooter.shoot(0.0);
 		intake.rollerStop();
 	}
 
-	@Override
 	protected void interrupted() {
 		end();
 	}
